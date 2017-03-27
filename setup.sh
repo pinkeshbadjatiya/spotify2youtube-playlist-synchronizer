1dac671850d35303c575fb4500bdbeafafd0b5a2
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2017-03-27 18:42:30 +0530

    Working setup.sh

--- setup.sh
+++ setup.sh
@@ -1,34 +1,56 @@
 sudo apt-get update
 sudo apt-get install python-dev python-pip nginx uwsgi uwsgi-plugin-python
-pip install -U -r example_configurations/requirements.txt
-cd /home/ubuntu/public_html/api.armalcolite.ml
-mkdir configs
+pip install -U -r ./example_configurations/requirements.txt
+rm -rf configs && mkdir configs
 cp ./example_configurations/config.json.example ./configs/config.json
 cp ./example_configurations/api-listener.service.example ./configs/api-listener.service
 cp ./example_configurations/api-listener.nginx.conf.example ./configs/api-listener.nginx.conf
 cp ./example_configurations/client_secret_api-listener.json ./configs/client_secret_api-listener.json
 
-echo "#####################################################"
-read -p "Update all the configs and then press enter to continue the setup." AA
+echo '#################################################################################################'
+echo '#################################################################################################'
+echo '#####                                                                                       #####'
+echo '#####   Update all the configs and then press enter to continue the setup.                  #####'
+echo '#####                                                                                       #####'
+echo '#####   1. Replace "<--APP_DIR--->" with the name of the folder in which the api-listener   #####'
+echo '#####      is cloned.                                                                       #####'
+echo '#####   2. Replace the "<--DOMAIN-->" with your domain name.                                #####'
+echo '#####   3. Read all the config files in "<--APP_DIR--->/configs/" and make necessary        #####'
+echo '#####      changes, especially the ones with replacing the "<--VARS-->".                    #####'
+echo '#####                                                                                       #####'
+echo '#####   >> Once Done pres any key to continue...                                            #####'
+echo '#####                                                                                       #####'
+echo '#################################################################################################'
+read temp
 
 # If you are running it for the first time then run using the below command. This will save the google-auth-credentials in a file
 # Try to add a song. Then it will save the credentials
+echo '#######################################################################################################'
+echo '#######################################################################################################'
+echo '#####                                                                                             #####'
+echo '#####   >> Starting a temporary server to save the google-auth-credentials in file                #####'
+echo '#####      "googlecredentials-oauth2.json".                                                       #####'
+echo '#####   >> Now open "http://localhost:8080/spotify2youtube?song=reality%20-%20lost%20frequencies  #####'
+echo '#####      and the uwsgi will print a URL in logs which needs to be accessed to save the          #####'
+echo '#####      credentials. It will then create file "googlecredentials-oauth2.json".                 #####'
+echo '#####   >> Now press Ctrl+C once to kill the uwsgi instance and continue the setup.               #####'
+echo '#####                                                                                             #####'
+echo '#######################################################################################################'
 uwsgi --socket 0.0.0.0:8080 --protocol=http api-listener.ini
 
-# Now we need to start the app using upstart
+# To start the app using upstart
 #sudo ln -s $PWD/configs/api-listener.upstart.conf /etc/init/    # Create a soft link for upstart in /etc/init
 #sudo initctl reload-configuration   # Upstart does not reload configurations automatically for soft-links, so do it manually
 #sudo start api-listener.service             # Start the api-listener app
 
+# To start the app using systemd
 sudo ln -s $PWD/configs/api-listener.service /etc/systemd/system/api-listener.service
 sudo systemctl daemon-reload        # Reload systemctl cache
 sudo systemctl start api-listener
 sudo systemctl status api-listener
 sudo systemctl enable api-listener
 
-
 # Now attach the uwsgi app with nginx
 # NOTE: Make sure you do not have anything else running on port 80
 sudo ln -s $PWD/configs/api-listener.nginx.conf /etc/nginx/sites-enabled/
 sudo service nginx restart
-
